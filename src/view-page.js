178d5236a248b1da1326adc70305c49d57001de9
Benno Lang <transmogribenno@gmail.com>
2023-01-09 17:21:26 +1030

    Reindent
    (This was once in an anonymous function)

--- src/view-page.js
+++ src/view-page.js
@@ -1,17 +1,17 @@
-    if (body) {
-        body.addEventListener('keydown', handleKeyDown);
-        body.addEventListener('click', handleClick);
-        body.addEventListener('mouseup', handleMouseUp);
-        body.addEventListener('mouseout', function(event) {
-            if (nodeIsInDefinition(event.target)) {
-                return;
-            }
-        });
+if (body) {
+    body.addEventListener('keydown', handleKeyDown);
+    body.addEventListener('click', handleClick);
+    body.addEventListener('mouseup', handleMouseUp);
+    body.addEventListener('mouseout', function(event) {
+        if (nodeIsInDefinition(event.target)) {
+            return;
+        }
+    });
 
+    addAltText();
+
+    const observer = new MutationObserver(function() {
         addAltText();
-
-        const observer = new MutationObserver(function() {
-            addAltText();
-        });
-        observer.observe(document, {subtree: true, childList: true});
-    }
+    });
+    observer.observe(document, {subtree: true, childList: true});
+}
